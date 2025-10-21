3fc23b35418242cfe29ea80850db972313df8419
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-21 10:42:43 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Suspense } from 'react';
 
 import './App.css'
 
 import Countries from './components/Countries/Countries.jsx';
+
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all').then(res=>res.json())
 function App() {
 
 
@@ -9,7 +13,10 @@ function App() {
     <>
      
       
-     <Countries></Countries>
+    <Suspense fallback={<p>Loading.....</p>}>
+      <Countries></Countries>
+
+    </Suspense>
       
       
     </>
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { use } from 'react';
 
-const countries = () => {
+const countries = ({countriesPromise}) => {
+    const countries =use(countriesPromise);
+    console.log(countries)
     return (
         <div>
             <h1>Welcome to country</h1>
