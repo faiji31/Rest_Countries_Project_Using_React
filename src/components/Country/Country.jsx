d910e83dc21cf11abbe6e4a9dfb346bf8bfcb809
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-23 00:14:36 +0600

    7th commit

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    console.log(country.name.common)
+    console.log(country)
     return (
         <div>
             <img src={country.flags.flags.png} alt="" />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Capital: {country.capital.capital}</p>
         </div>
     );
 };
